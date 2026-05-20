96270dcc8828bf070ae18ded366988eb57e820a8
Lasse <lassemostuen@gmail.com>
2026-05-20 19:37:58 +0200

    prosjekt start
    Hobbyprosjekt som skal vise hvilke bøker jeg har lest, har tilgjengelig, har på ønskelisten

---
+++ src/main/java/com/lasse/bokhylla/BokhyllaApplication.java
@@ -0,0 +1,13 @@
+package com.lasse.bokhylla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokhyllaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BokhyllaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lasse/bokhylla/BokhyllaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasse.bokhylla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokhyllaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
